03cd087c1709a09b82bf8a27f97300f6f4a6863e
Toonyloo <tony0818abc@gmail.com>
2021-12-22 20:52:01 -0500

    Refactored code and implemented a bomb feature

--- game.py
+++ game.py
@@ -3,11 +3,18 @@ pygame.init()
 
 WIDTH = 600
 HEIGHT = 800
-SHIPWIDTH = 80
-SHIPHEIGHT = 80
-BULLETWIDTH = 15
-BULLETHEIGHT = 30
-HPBARHEIGHT = 20
+SHIP_WIDTH = 80
+SHIP_HEIGHT = 80
+BULLET_WIDTH = 15
+BULLET_HEIGHT = 30
+HP_BAR_HEIGHT = 20
+BOMB_WIDTH = 35
+BOMB_HEIGHT = 50
+EXPLOSION_SIZE = 175
+DETONATION_TIME = 120
+BOMB_DMG = 0.3
+LASER_DMG = 5
+
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
@@ -17,37 +24,52 @@ FPS = 60
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 
 SHIP1 = pygame.image.load('assets/ship1.png').convert_alpha()
-SHIP1 = pygame.transform.scale(SHIP1, (SHIPWIDTH, SHIPHEIGHT))
+SHIP1 = pygame.transform.scale(SHIP1, (SHIP_WIDTH, SHIP_HEIGHT))
 
 SHIP2 = pygame.image.load('assets/ship2.png').convert_alpha()
-SHIP2 = pygame.transform.scale(SHIP2, (SHIPWIDTH, SHIPHEIGHT))
+SHIP2 = pygame.transform.scale(SHIP2, (SHIP_WIDTH, SHIP_HEIGHT))
 
 LASER1 = pygame.image.load('assets/laser1.png').convert_alpha()
-LASER1 = pygame.transform.scale(LASER1, (BULLETWIDTH, BULLETHEIGHT))
+LASER1 = pygame.transform.scale(LASER1, (BULLET_WIDTH, BULLET_HEIGHT))
 
 LASER2 = pygame.image.load('assets/laser2.png').convert_alpha()
-LASER2 = pygame.transform.scale(LASER2, (BULLETWIDTH, BULLETHEIGHT))
+LASER2 = pygame.transform.scale(LASER2, (BULLET_WIDTH, BULLET_HEIGHT))
 
-BACKGROUND = pygame.image.load('assets/background.jpg').convert()
+BOMB1 = pygame.image.load('assets/bomb1.png').convert_alpha()
+BOMB1 = pygame.transform.scale(BOMB1, (BOMB_WIDTH, BOMB_HEIGHT))
+
+BOMB2 = pygame.image.load('assets/bomb2.png').convert_alpha()
+BOMB2 = pygame.transform.scale(BOMB2, (BOMB_WIDTH, BOMB_HEIGHT))
+
+BACKGROUND = pygame.image.load('assets/background.png').convert_alpha()
 BACKGROUND = pygame.transform.scale(BACKGROUND, (WIDTH, HEIGHT))
 BACKGROUND.set_alpha(75)
 
 EXPLOSION = []
 for i in range(1, 26):
     explosion = pygame.image.load(f'assets/explosion/{i}.png').convert_alpha()
-    explosion = pygame.transform.scale(explosion, (SHIPWIDTH, SHIPWIDTH))
+    explosion = pygame.transform.scale(explosion, (SHIP_WIDTH, SHIP_WIDTH))
     EXPLOSION.append(explosion)
 
-LASERSFX = pygame.mixer.Sound('assets/lasershoot.wav')
-LASERSFX.set_volume(0.2)
+LASER_SFX = pygame.mixer.Sound('assets/lasershoot.wav')
+LASER_SFX.set_volume(0.2)
 
-EXPLOSIONSFX = pygame.mixer.Sound('assets/explosion.wav')
-EXPLOSIONSFX.set_volume(0.2)
+EXPLOSION_SFX = pygame.mixer.Sound('assets/explosion.wav')
+EXPLOSION_SFX.set_volume(0.2)
+
+DAMAGE_SFX = pygame.mixer.Sound('assets/damage.wav')
+DAMAGE_SFX.set_volume(0.2)
+
+BOMB_SHOOT_SFX = pygame.mixer.Sound('assets/bombshoot.wav')
+BOMB_SHOOT_SFX.set_volume(0.2)
+
+BOMB_DETONATE_SFX = pygame.mixer.Sound('assets/bombdetonate.wav')
+BOMB_DETONATE_SFX.set_volume(0.2)
 
 FONT = pygame.font.Font('assets/PressStart2P.ttf', 20)
 
 pygame.display.set_caption('Spaceships')
-pygame.display.set_icon(SHIP1)
+pygame.display.set_icon(pygame.image.load('assets/icon.png'))
 
 clock = pygame.time.Clock()
 
@@ -56,129 +78,307 @@ class Player1(pygame.sprite.Sprite):
     def __init__(self):
         super(Player1, self).__init__()
         self.image = SHIP1
-        self.x = WIDTH / 2 - SHIPWIDTH / 2
-        self.y = (HEIGHT - HEIGHT // 4) - SHIPHEIGHT
-        self.xspeed = 0
-        self.yspeed = 0
+        self.x = WIDTH / 2 - SHIP_WIDTH / 2
+        self.y = (HEIGHT - HEIGHT // 4) - SHIP_HEIGHT
+        self.x_speed = 0
+        self.y_speed = 0
         self.health = 100
         self.ammo = 10
+        self.bullets = []
+        self.bomb = None
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
 
     def handle_input(self):
-        self.xspeed = 0
-        self.yspeed = 0
+        self.x_speed = 0
+        self.y_speed = 0
         if keys[pygame.K_w]:
-            self.yspeed -= 10
+            self.y_speed -= 10
         if keys[pygame.K_s]:
-            self.yspeed += 10
+            self.y_speed += 10
         if keys[pygame.K_a]:
-            self.xspeed -= 10
+            self.x_speed -= 10
         if keys[pygame.K_d]:
-            self.xspeed += 10
+            self.x_speed += 10
+
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if self.ammo > 0:
+                        self.ammo -= 1
+                        LASER_SFX.play()
+                        self.bullets.append(Bullet1((self.x, self.y)))
+                if event.key == pygame.K_f:
+                    if self.bomb is None:
+                        self.bomb = Bomb1((self.x, self.y))
+                    else:
+                        self.bomb.trigger_explosion()
 
     def handle_movement(self):
-        if 0 <= (self.xspeed + self.x) <= WIDTH - SHIPWIDTH:
-            self.x += self.xspeed
-        if HEIGHT / 2 <= (self.yspeed + self.y) <= HEIGHT - SHIPHEIGHT - HPBARHEIGHT:
-            self.y += self.yspeed
+        if 0 <= (self.x_speed + self.x) <= WIDTH - SHIP_WIDTH:
+            self.x += self.x_speed
+        if HEIGHT / 2 <= (self.y_speed + self.y) <= HEIGHT - SHIP_HEIGHT - HP_BAR_HEIGHT:
+            self.y += self.y_speed
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
 
     def draw(self):
         screen.blit(self.image, (self.x, self.y))
 
-    def draw_healthbar(self):
-        hpsection = WIDTH * self.health / 100
-        remaininghp = pygame.Rect(0, HEIGHT - HPBARHEIGHT, hpsection, HPBARHEIGHT)
-        missinghp = pygame.Rect(hpsection, HEIGHT - HPBARHEIGHT, WIDTH - hpsection, HPBARHEIGHT)
-        pygame.draw.rect(screen, GREEN, remaininghp)
-        pygame.draw.rect(screen, RED, missinghp)
+    def handle_bullets(self):
+        for count, bullet in enumerate(self.bullets):
+            bullet.handle_movement()
+            if pygame.sprite.collide_mask(bullet, p2):
+                DAMAGE_SFX.play()
+                p2.health -= LASER_DMG
+                del self.bullets[count]
+                continue
+            if bullet.y < 0:
+                del self.bullets[count]
+                continue
+
+    def handle_bomb(self):
+        if self.bomb is None:
+            return
+        if self.bomb.y < 0 - BOMB_HEIGHT:
+            self.bomb = None
+            return
+
+        if self.bomb.detonation is None:
+            self.bomb.handle_movement()
+            if pygame.sprite.collide_mask(self.bomb, p2):
+                self.bomb.trigger_explosion()
+        elif self.bomb.detonation > 0:
+            self.bomb.explosion()
+            if pygame.sprite.collide_mask(self.bomb, p1):
+                p1.health -= BOMB_DMG
+            if pygame.sprite.collide_mask(self.bomb, p2):
+                p2.health -= BOMB_DMG
+        elif self.bomb.detonation == 0:
+            self.bomb = None
 
 
 class Player2(pygame.sprite.Sprite):
     def __init__(self):
         super(Player2, self).__init__()
         self.image = SHIP2
-        self.x = WIDTH / 2 - SHIPWIDTH / 2
+        self.x = WIDTH / 2 - SHIP_WIDTH / 2
         self.y = HEIGHT // 4
-        self.xspeed = 0
-        self.yspeed = 0
+        self.x_speed = 0
+        self.y_speed = 0
         self.health = 100
         self.ammo = 10
+        self.bullets = []
+        self.bomb = None
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
 
     def handle_input(self):
-        self.xspeed = 0
-        self.yspeed = 0
+        self.x_speed = 0
+        self.y_speed = 0
         if keys[pygame.K_UP]:
-            self.yspeed -= 10
+            self.y_speed -= 10
         if keys[pygame.K_DOWN]:
-            self.yspeed += 10
+            self.y_speed += 10
         if keys[pygame.K_LEFT]:
-            self.xspeed -= 10
+            self.x_speed -= 10
         if keys[pygame.K_RIGHT]:
-            self.xspeed += 10
+            self.x_speed += 10
+
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RSHIFT:
+                    if self.ammo > 0:
+                        self.ammo -= 1
+                        LASER_SFX.play()
+                        self.bullets.append(Bullet2((self.x, self.y)))
+                if event.key == pygame.K_SLASH:
+                    if self.bomb is None:
+                        self.bomb = Bomb2((self.x, self.y))
+                    else:
+                        self.bomb.trigger_explosion()
 
     def handle_movement(self):
-        if 0 <= (self.xspeed + self.x) <= WIDTH - SHIPWIDTH:
-            self.x += self.xspeed
-        if HPBARHEIGHT <= (self.yspeed + self.y) <= (HEIGHT / 2) - SHIPHEIGHT:
-            self.y += self.yspeed
+        if 0 <= (self.x_speed + self.x) <= WIDTH - SHIP_WIDTH:
+            self.x += self.x_speed
+        if HP_BAR_HEIGHT <= (self.y_speed + self.y) <= (HEIGHT / 2) - SHIP_HEIGHT:
+            self.y += self.y_speed
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
 
     def draw(self):
         screen.blit(self.image, (self.x, self.y))
 
-    def draw_healthbar(self):
-        hpsection = WIDTH * self.health / 100
-        remaininghp = pygame.Rect(0, 0, hpsection, HPBARHEIGHT)
-        missinghp = pygame.Rect(hpsection, 0, WIDTH - hpsection, HPBARHEIGHT)
-        pygame.draw.rect(screen, GREEN, remaininghp)
-        pygame.draw.rect(screen, RED, missinghp)
+    def handle_bullets(self):
+        for count, bullet in enumerate(self.bullets):
+            bullet.handle_movement()
+            if pygame.sprite.collide_mask(bullet, p1):
+                DAMAGE_SFX.play()
+                p1.health -= LASER_DMG
+                del self.bullets[count]
+                continue
+            if bullet.y > HEIGHT + BULLET_HEIGHT:
+                del self.bullets[count]
+
+    def handle_bomb(self):
+        if self.bomb is None:
+            return
+        if self.bomb.y > HEIGHT:
+            self.bomb = None
+            return
+
+        if self.bomb.detonation is None:
+            self.bomb.handle_movement()
+            if pygame.sprite.collide_mask(self.bomb, p1):
+                self.bomb.trigger_explosion()
+        elif self.bomb.detonation > 0:
+            self.bomb.explosion()
+            if pygame.sprite.collide_mask(self.bomb, p1):
+                p1.health -= BOMB_DMG
+            if pygame.sprite.collide_mask(self.bomb, p2):
+                p2.health -= BOMB_DMG
+        elif self.bomb.detonation == 0:
+            self.bomb = None
 
 
 class Bullet1(pygame.sprite.Sprite):
-    def __init__(self, ship):
+    def __init__(self, coords):
         super(Bullet1, self).__init__()
         self.image = LASER1
-        self.x = ship.x + (SHIPWIDTH - BULLETWIDTH) / 2
-        self.y = ship.y
+        self.x = coords[0] + (SHIP_WIDTH - BULLET_WIDTH) / 2
+        self.y = coords[1]
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
 
     def handle_movement(self):
-        bullet.y -= 15
+        self.y -= 15
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
+
+    def draw(self):
+        screen.blit(self.image, (self.x, self.y))
 
 
 class Bullet2(pygame.sprite.Sprite):
-    def __init__(self, ship):
+    def __init__(self, coords):
         super(Bullet2, self).__init__()
         self.image = LASER2
-        self.x = ship.x + (SHIPWIDTH - BULLETWIDTH) / 2
-        self.y = ship.y
+        self.x = coords[0] + (SHIP_WIDTH - BULLET_WIDTH) / 2
+        self.y = coords[1]
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
 
     def handle_movement(self):
-        bullet.y += 15
+        self.y += 15
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
+
+    def draw(self):
+        screen.blit(self.image, (self.x, self.y))
+
+
+class Bomb1(pygame.sprite.Sprite):
+    def __init__(self, coords):
+        super(Bomb1, self).__init__()
+        self.image = BOMB1
+        self.x = coords[0] + (SHIP_WIDTH - BOMB_WIDTH) / 2
+        self.y = coords[1]
+        self.detonation = None
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+        BOMB_SHOOT_SFX.play()
+
+    def handle_movement(self):
+        self.y -= 8
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+
+    def explosion(self):
+        self.detonation -= 1
+        frame = (self.detonation % 4) + 11
+        image = EXPLOSION[frame]
+        image = pygame.transform.scale(image, (EXPLOSION_SIZE, EXPLOSION_SIZE))
+        self.image = image
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+
+    def trigger_explosion(self):
+        if self.detonation is None:
+            BOMB_DETONATE_SFX.play()
+            self.x -= EXPLOSION_SIZE / 2
+            self.y -= EXPLOSION_SIZE / 2
+            self.detonation = DETONATION_TIME
+
+    def draw(self):
+        screen.blit(self.image, (self.x, self.y))
+
+
+class Bomb2(pygame.sprite.Sprite):
+    def __init__(self, coords):
+        super(Bomb2, self).__init__()
+        self.image = BOMB2
+        self.x = coords[0] + (SHIP_WIDTH - BOMB_WIDTH) / 2
+        self.y = coords[1]
+        self.detonation = None
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+        BOMB_SHOOT_SFX.play()
+
+    def handle_movement(self):
+        self.y += 8
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+
+    def explosion(self):
+        self.detonation -= 1
+        frame = (self.detonation % 4) + 11
+        image = EXPLOSION[frame]
+        image = pygame.transform.scale(image, (EXPLOSION_SIZE, EXPLOSION_SIZE))
+        self.image = image
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+
+    def trigger_explosion(self):
+        if self.detonation is None:
+            BOMB_DETONATE_SFX.play()
+            self.x -= EXPLOSION_SIZE / 2
+            self.y -= EXPLOSION_SIZE / 2
+            self.detonation = DETONATION_TIME
+
+    def draw(self):
+        screen.blit(self.image, (self.x, self.y))
 
 
 def draw_game():
     screen.fill(BLACK)
     screen.blit(BACKGROUND, (0, 0))
     pygame.draw.line(screen, WHITE, (0, HEIGHT/2), (WIDTH, HEIGHT/2))
-    p1.draw_healthbar()
-    p2.draw_healthbar()
-    p1.draw()
-    p2.draw()
+
+    hp_section = WIDTH * p1.health / 100
+    remaining_hp = pygame.Rect(
+        0, HEIGHT - HP_BAR_HEIGHT, hp_section, HP_BAR_HEIGHT)
+    missing_hp = pygame.Rect(hp_section, HEIGHT -
+                             HP_BAR_HEIGHT, WIDTH - hp_section, HP_BAR_HEIGHT)
+    pygame.draw.rect(screen, GREEN, remaining_hp)
+    pygame.draw.rect(screen, RED, missing_hp)
+
+    hp_section = WIDTH * p2.health / 100
+    remaining_hp = pygame.Rect(0, 0, hp_section, HP_BAR_HEIGHT)
+    missing_hp = pygame.Rect(
+        hp_section, 0, WIDTH - hp_section, HP_BAR_HEIGHT)
+    pygame.draw.rect(screen, GREEN, remaining_hp)
+    pygame.draw.rect(screen, RED, missing_hp)
+
     p1_ammo_txt = FONT.render(f'Ammo:{p1.ammo}', True, WHITE)
     p2_ammo_txt = FONT.render(f'Ammo:{p2.ammo}', True, WHITE)
-    p1_ammo_width, p1_ammo_height= FONT.size(f'Ammo:{p1.ammo}')
-    screen.blit(p1_ammo_txt, (WIDTH - p1_ammo_width, HEIGHT - HPBARHEIGHT - p1_ammo_height))
-    screen.blit(p2_ammo_txt, (0, HPBARHEIGHT))
+    p1_ammo_width, p1_ammo_height = FONT.size(f'Ammo:{p1.ammo}')
+    p2_ammo_width, p2_ammo_height = FONT.size(f'Ammo:{p2.ammo}')
+    screen.blit(p1_ammo_txt, (WIDTH - p1_ammo_width,
+                HEIGHT - HP_BAR_HEIGHT - p1_ammo_height))
+    screen.blit(p2_ammo_txt, (WIDTH - p2_ammo_width, HP_BAR_HEIGHT))
+
+    for bullet in p1.bullets:
+        bullet.draw()
+    for bullet in p2.bullets:
+        bullet.draw()
+
+    if p1.bomb is not None:
+        p1.bomb.draw()
+    if p2.bomb is not None:
+        p2.bomb.draw()
+
+    p1.draw()
+    p2.draw()
 
 
 def explosion_animation(ship):
-    EXPLOSIONSFX.play()
+    EXPLOSION_SFX.play()
     for frame in EXPLOSION:
         clock.tick(FPS//5)
         draw_game()
@@ -189,83 +389,38 @@ def explosion_animation(ship):
 running = True
 p1 = Player1()
 p2 = Player2()
-p1bullets = []
-p2bullets = []
-ammotimer = 0
+ammo_timer = 0
 
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
     clock.tick(FPS)
 
-    if p1.health <= 0:
-        explosion_animation(p1)
-        p1 = Player1()
-        p2 = Player2()
-        p1bullets = []
-        p2bullets = []
-        continue
-    if p2.health <= 0:
-        explosion_animation(p2)
-        p1 = Player1
-        p1 = Player1()
-        p2 = Player2()
-        p1bullets = []
-        p2bullets = []
-        continue
-    
-    if ammotimer < 30:
-        ammotimer += 1
+    keys = pygame.key.get_pressed()
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            running = False
+
+    if ammo_timer < 30:
+        ammo_timer += 1
     else:
-        ammotimer = 0
+        ammo_timer = 0
         if p1.ammo < 10:
             p1.ammo += 1
         if p2.ammo < 10:
             p2.ammo += 1
 
-    keys = pygame.key.get_pressed()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                if p1.ammo > 0:
-                    p1.ammo -= 1
-                    LASERSFX.play()
-                    p1bullets.append(Bullet1(p1))
-            if event.key == pygame.K_RSHIFT:
-                if p2.ammo > 0:
-                    p2.ammo -= 1
-                    LASERSFX.play()
-                    p2bullets.append(Bullet2(p2))
+    for player in (p1, p2):
+        if player.health <= 0:
+            explosion_animation(player)
+            p1 = Player1()
+            p2 = Player2()
+            continue
 
-    p1.handle_input()
-    p2.handle_input()
-    p1.handle_movement()
-    p2.handle_movement()
+        player.handle_input()
+        player.handle_movement()
+        player.handle_bullets()
+        player.handle_bomb()
 
     draw_game()
-
-    for count, bullet in enumerate(p1bullets):
-        bullet.handle_movement()
-        if pygame.sprite.collide_mask(bullet, p2):
-            p2.health -= 5
-            del p1bullets[count]
-            continue
-        if bullet.y < 0:
-            del p1bullets[count]
-            continue
-        screen.blit(bullet.image, (bullet.x, bullet.y))
-
-    for count, bullet in enumerate(p2bullets):
-        bullet.handle_movement()
-        if pygame.sprite.collide_mask(bullet, p1):
-            p1.health -= 5
-            del p2bullets[count]
-            continue
-        screen.blit(bullet.image, (bullet.x, bullet.y))
-        if bullet.y > HEIGHT + BULLETHEIGHT:
-            del p2bullets[count]
 
     pygame.display.flip()
